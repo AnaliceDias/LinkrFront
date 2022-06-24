82fe22b40a1c6352f2dea09036d27b70475f302f
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-06-24 16:30:43 -0300

    changed css of trendings box

--- src/components/organizingBoxes.js
+++ src/components/organizingBoxes.js
@@ -9,7 +9,7 @@ const BoxPage = styled.div`
   margin-top: 72px;
 
   width: 100%;
-  heigth: 100%;
+  height: 100%;
 
 `
 
@@ -26,17 +26,18 @@ const Container = styled.div`
   position: relative;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: top;
   width: 80%;
-  heigth: 100%;
+  height: 100%;
 
-  @media (min-width: 1001px , max-width: 1500px) {
+  @media (min-width: 1101px) {
     width: 90%;
+    max-width: 1000px;
   }
 
 
-  @media (max-width: 1000px) {
+  @media (max-width: 1100px) {
     width: 100%;
     justify-content: center;
   }
